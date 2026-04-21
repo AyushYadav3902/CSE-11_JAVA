6184114ffc0cf0b1dc8932936421e6c425ed9857
AyushYadav3902 <jaglarayush4091@gmail.com>
2026-04-21 10:29:32 +0530

    21/04/26

---
+++ Students.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Students() {
+  return (
+    <div>
+      <h1>Hello Mates Name is {CSSStyleProperties.name}</h1>
+    </div>
+  )
+}
+
+export default Student
